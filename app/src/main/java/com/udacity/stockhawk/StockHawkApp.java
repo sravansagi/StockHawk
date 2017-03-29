df3460a88013a0d68d4a9f03027bec2c7b1ec18e
Sravan <venkatasravan13@gmail.com>
2017-03-29 16:04:37 +0530

    Initial Commit

--- app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
+++ app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
@@ -2,6 +2,8 @@ package com.udacity.stockhawk;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 import timber.log.Timber;
 
 public class StockHawkApp extends Application {
@@ -9,6 +11,7 @@ public class StockHawkApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
 
         if (BuildConfig.DEBUG) {
             Timber.uprootAll();
